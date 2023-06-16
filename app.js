7d3c2f8ff793a976fd2269d9c5c7b98e6ed80e9b
kutaymalik <56682209+kutaymalik@users.noreply.github.com>
2023-06-16 18:16:01 +0300

    Static files added.

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-const fn = () => {
-  console.log('Function working');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+const myLogger = (req, res, next) => {
+    console.log('Middleware log 1');
+    next();
 };
+
+const myLogger2 = (req, res, next) => {
+    console.log('Middleware log 2');
+    next();
+};
+
+// MIDDLEWARES
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server ${port} was started.`);
+});
